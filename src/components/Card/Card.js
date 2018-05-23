fc333766bb41daf982f96debeda23fc514a0a752
Matthew Atkins <matkinsdesigns@gmail.com>
2018-05-23 15:14:44 -0400

    imported json file to card component

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import list from '../../InterviewProject.json'
 
 class Card extends Component {
   render () {
     return (
       <div>
-        hello
+        {list.map(data =>
+          <ol>{data.Heading}</ol>
+        )}
       </div>
     )
   }
