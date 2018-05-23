a8a9443ed447d8bc72a8a664f3097ac9d9d621c6
Matthew Atkins <matkinsdesigns@gmail.com>
2018-05-23 18:19:54 -0400

    base card structure and layout

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -8,11 +8,13 @@ class Card extends Component {
       <div className='card-container'>
         {list.map(data =>
           <div className='card-wrapper'>
-            {data.showBridge ? <img src='http://res.cloudinary.com/bguggie/image/upload/v1425514888/wide_ggbridge_bg_teneax.jpg' alt='bridge' /> : <img src='http://res.cloudinary.com/bguggie/image/upload/v1425514736/place_holder_zuvywg.png' alt='placeholder' /> }
+            <div className='image-container'>
+              {data.showBridge ? <img src='http://res.cloudinary.com/bguggie/image/upload/v1425514888/wide_ggbridge_bg_teneax.jpg' alt='bridge' /> : <img className='placeholder-card' src='http://res.cloudinary.com/bguggie/image/upload/v1425514736/place_holder_zuvywg.png' alt='placeholder' /> }
+            </div>
             <h5>{data.Heading}</h5>
             <span>{data.Subheading}</span>
             <hr />
-            <span>{data.Price}</span>
+            <span className='price'>{data.Price}</span>
           </div>
         )}
       </div>
