6e973bf7a19e6ffd8925c75660f34c7452814d5d
Matthew Atkins <matkinsdesigns@gmail.com>
2018-05-23 22:06:03 -0400

    updates to card layouts

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -9,12 +9,16 @@ class Card extends Component {
         {list.map(data =>
           <div className='card-wrapper'>
             <div className='image-container'>
-              {data.showBridge ? <img src='http://res.cloudinary.com/bguggie/image/upload/v1425514888/wide_ggbridge_bg_teneax.jpg' alt='bridge' /> : <img className='placeholder-card' src='http://res.cloudinary.com/bguggie/image/upload/v1425514736/place_holder_zuvywg.png' alt='placeholder' /> }
+              {data.showBridge ? <img className='bridge-card' src='http://res.cloudinary.com/bguggie/image/upload/v1425514888/wide_ggbridge_bg_teneax.jpg' alt='bridge' /> : <img className='placeholder-card' src='http://res.cloudinary.com/bguggie/image/upload/v1425514736/place_holder_zuvywg.png' alt='placeholder' /> }
             </div>
-            <h5>{data.Heading}</h5>
-            <span>{data.Subheading}</span>
-            <hr />
-            <span className='price'>{data.Price}</span>
+            <div className='content-container'>
+              <h4 className='text'>{data.Heading}</h4>
+              <span className='text'>{data.Subheading}</span>
+              {/* <hr className='hr' /> */}
+              <div className='price-container'>
+                <span className='price'>{'$' + data.Price.toLocaleString()}</span>
+              </div>
+            </div>
           </div>
         )}
       </div>
